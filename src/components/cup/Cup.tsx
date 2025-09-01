192a3302602bd636e14fe94e9f886a9d307c11c9
David Eastmond <davebuck1981@gmail.com>
2025-09-01 08:49:32 -0400

    refactor: consolidate colorMap definition and improve Cup component logic

--- src/components/cup/Cup.tsx
+++ src/components/cup/Cup.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import type { CupColor } from "../../types/cup-color";
+import { colorMap, type CupColor } from "../../types/cup-color";
 import "./style.css";
 type CupProps = {
   color: CupColor;
@@ -10,17 +10,6 @@ type CupProps = {
 
 export default function Cup(props: CupProps) {
   const backgroundColorFromProps = useMemo<string>(() => {
-    const colorMap = {
-      blue: "bg-blue-500",
-      red: "bg-red-500",
-      green: "bg-green-500",
-      yellow: "bg-yellow-500",
-      purple: "bg-purple-500",
-      white: "bg-white",
-      pink: "bg-pink-500",
-      orange: "bg-orange-500",
-    };
-
     return (colorMap[props.color] as string) || "bg-white";
   }, []);
 
@@ -31,12 +20,14 @@ export default function Cup(props: CupProps) {
   return (
     <div
       id={props.color}
-      className={`cup hover:cursor-pointer ${backgroundColorFromProps} ${
+      className={`cup hover:cursor-pointer ${backgroundColorFromProps} flex ${
         props.selected
-          ? "border-4 border-yellow-100 ring-4 ring-yellow-100 animate-bounce"
+          ? "border-[1.5px] border-purple-100 ring-1 ring-purple-100 animate-bounce"
           : ""
       } ${props.disabled ? "opacity-50 pointer-events-none" : ""}`}
       onClick={handleSelected}
-    ></div>
+    >
+      <div className="h-[9.5px] bg-white w-full self-end border-black border-1"></div>
+    </div>
   );
 }
--- src/types/cup-color.ts
+++ src/types/cup-color.ts
@@ -7,3 +7,14 @@ export type CupColor =
   | "purple"
   | "blue"
   | "pink";
+
+export const colorMap = {
+  blue: "bg-blue-500",
+  red: "bg-red-500",
+  green: "bg-green-500",
+  yellow: "bg-yellow-500",
+  purple: "bg-purple-500",
+  white: "bg-white",
+  pink: "bg-pink-500",
+  orange: "bg-orange-500",
+};
